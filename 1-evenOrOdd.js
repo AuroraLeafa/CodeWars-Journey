1084286f85ed7fd4233244641517188568928fe0
Refansyach <revansahdr@gmail.com>
2024-02-03 23:05:49 +0700

    Day 1

--- 1-evenOrOdd.js
+++ 1-evenOrOdd.js
@@ -1,12 +1,12 @@
 /* 
     Day 1 - Code Wars Challenge
     Link : https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/
-    CLEAR!
+    CLEAR!!
 */
 
 // function evenOrOdd(number) {
 //   return number % 2 == 0 ? "Even" : "Odd";
-// }
+// } 
 
 const evenOrOdd = number => number % 2 === 0 ? "Even" : "Odd";
 
