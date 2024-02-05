ccf6bae56067c4e6b5c805f7ac89f4e85fb99dfe
Refansyach <revansahdr@gmail.com>
2024-02-05 17:52:05 +0700

    Day 3 - Done!

---
+++ 3-makeNegative.js
@@ -0,0 +1,11 @@
+/* 
+    Day 3 - Code Wars Challenge
+    Link : https://www.codewars.com/kata/55685cd7ad70877c23000102
+    CLEAR!!
+*/
+
+const makeNegative = num => num > 0 ? -num : num;
+
+console.log(makeNegative(5));
+console.log(makeNegative(-5));
+console.log(makeNegative(0));
