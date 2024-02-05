525ff7984b707d80ef8984d45aafd3d9c7d485a1
Refansyach <revansahdr@gmail.com>
2024-02-05 22:58:23 +0700

    Day 3 pt. 2 - Clear!

---
+++ 3-multiply.js
@@ -0,0 +1,12 @@
+/* 
+    Day 3.5 - Code Wars Challenge
+    Link : https://www.codewars.com/kata/50654ddff44f800200000004
+    CLEAR!!
+*/
+
+// function multiply(a, b){
+//     return a * b
+// }
+
+const multiply = (a, b) => a*b;
+   
