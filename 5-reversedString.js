3f1ca66a05ae441ee846427fde2cac4aa44ba2c6
Refansyach <revansahdr@gmail.com>
2024-02-08 22:26:12 +0700

    Day 5 - Clear!

---
+++ 5-reversedString.js
@@ -0,0 +1,11 @@
+/* 
+    Day 5 - Code Wars Challenge
+    Link : https://www.codewars.com/kata/5168bb5dfe9a00b126000018/
+    CLEAR!!
+*/
+
+// function solution(str){
+//     return str.split("").reverse("").join("")
+//   }
+
+const solution = str => str.split("").reverse("").join("");
