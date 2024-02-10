8ffa65339b566b830605aa04a4bf634c03974f69
Refansyach <revansahdr@gmail.com>
2024-02-10 21:25:48 +0700

    Day 7 Pt. 2 - Clear!

---
+++ 7-oppositeInt.js
@@ -0,0 +1,13 @@
+/* 
+    Day 7 - Code Wars Challenge
+    Link : https://www.codewars.com/kata/56dec885c54a926dcd001095/
+    CLEAR!!
+*/
+
+// function opposite(number) {
+//     return number *= -1;
+// }
+
+const opposite = number => -number;
+
+console.log(opposite(241));
