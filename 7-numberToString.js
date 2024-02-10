08510a86c8b1576356c55d4b66c7119bb561d0ca
Refansyach <revansahdr@gmail.com>
2024-02-10 17:23:22 +0700

    Day 7 - Clear!

---
+++ 7-numberToString.js
@@ -0,0 +1,13 @@
+/* 
+    Day 7 - Code Wars Challenge
+    Link : https://www.codewars.com/kata/5265326f5fda8eb1160004c8/
+    CLEAR!!
+*/
+
+// function numberToString(num) {
+//    return String(num)
+//   }
+
+const numberToString = num => num.toString();
+
+  console.log(numberToString(67));
