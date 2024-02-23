d589d4578eeec2beaa214a0d02f1c18de4cf83af
Refansyach <revansahdr@gmail.com>
2024-02-23 19:19:29 +0700

    Day 21 - Clear!

---
+++ 21-lostMap.js
@@ -0,0 +1,17 @@
+/* 
+    Day 20 - Code Wars Challenge
+    Link : https://www.codewars.com/kata/57f781872e3d8ca2a000007e/
+    CLEAR !!
+*/
+
+// function maps(x){
+//     let newArr = []
+//     for (let i = 0; i < x.length; i++) {
+//         newArr[i] = x[i] * 2
+//     }
+//     return newArr
+// }
+
+const maps = x => x.map(number => number * 2)
+
+console.log(maps([1,2,3]));
