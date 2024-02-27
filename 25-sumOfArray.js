52eb723fd8ef5f46785596ab85723293239b714e
Refansyach <revansahdr@gmail.com>
2024-02-27 10:01:16 +0700

    Day 25 Pt. 2 - Clear

---
+++ 25-sumOfArray.js
@@ -0,0 +1,17 @@
+/* 
+    Day 25 Pt. 2 - Code Wars Challenge
+    Link : https://www.codewars.com/kata/53dc54212259ed3d4f00071c/
+    CLEAR !!
+*/
+
+// function sum(numbers){
+//     let total = 0;
+//     for(const number of numbers){
+//         total += number
+//     }
+//     return total
+// }
+
+const sum = numbers => numbers.reduce((acc, curr) => acc + curr, 0);
+
+console.log(sum([1,2,3,4,5]));
