f18f56d5dcd8dc6c592796d77d0a7f2ed7e6e6a9
Refansyach <revansahdr@gmail.com>
2024-03-19 05:50:37 +0700

    Day 43 - Clear!

---
+++ 43-stringToArray.js
@@ -0,0 +1,13 @@
+/* 
+    Day 43 - Code Wars Challenge
+    Link : https://www.codewars.com/kata/57e76bc428d6fbc2d500036d/
+    CLEAR !!
+*/
+
+// function stringToArray(string){
+// 	return string.split(" ")
+// }
+
+const stringToArray = string => string.split(" ")
+
+console.log(stringToArray("Robin Singh"));
