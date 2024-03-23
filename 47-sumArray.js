cf2edf736a5f4c77a72015d1f3161474c528631c
Refansyach <revansahdr@gmail.com>
2024-03-23 12:20:53 +0700

    Day 47 - Clear!

---
+++ 47-sumArray.js
@@ -0,0 +1,20 @@
+/* 
+    Day 46 - Code Wars Challenge
+    Link : https://www.codewars.com/kata/576b93db1129fcf2200001e6/
+    CLEAR !!
+*/
+
+function sumArray(array) {
+  if (!array || array.length < 1) {
+    return 0;
+  }
+  let sum = 0;
+  for (const element of array) {
+    sum += element;
+  }
+  return sum - Math.min(...array) - Math.max(...array);
+}
+
+// const sumArray = array => !array || array.length < 1 ? 0 : array.sort((a,b) => a-b).slice(1,-1).reduce((acc, curr) => acc + curr, 0)
+
+console.log(sumArray([5, 3, 4, 2, 1, 1]));
