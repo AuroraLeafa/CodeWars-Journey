a295ff52e05a6e3817c57312531c3bb343206df7
Refansyach <revansahdr@gmail.com>
2024-03-26 13:31:47 +0700

    Day 49 - Clear!

---
+++ 49-getAverage.js
@@ -0,0 +1,17 @@
+/* 
+    Day 48 - Code Wars Challenge
+    Link : https://www.codewars.com/kata/563e320cee5dddcf77000158/
+    CLEAR !!
+*/
+
+const getAverage = marks => {
+    let avg = 0;
+    for (let i = 0; i < marks.length; i++) {
+        avg += marks[i]
+    }
+    return Math.floor(avg / marks.length)
+}
+
+// const getAverage = marks => Math.floor(marks.reduce((acc, curr) => acc + curr, 0) / marks.length)
+
+console.log(getAverage([1, 2, 3, 4, 5]));
