3a4da5427fdeb9ee174bbec6b3b48680da44dbba
Refansyach <revansahdr@gmail.com>
2024-04-08 07:53:04 +0700

    Day 61 - Clear!

---
+++ 61-cockroachSpeed.js
@@ -0,0 +1,13 @@
+/* 
+    Day 61 - Code Wars Challenge
+    Link : https://www.codewars.com/kata/55fab1ffda3e2e44f00000c6
+    CLEAR !!
+*/
+
+// function cockroachSpeed(s) {
+//   return Math.floor(s * 27.778);
+// }
+
+const cockroachSpeed = s => Math.floor(s * 27.778)
+
+console.log(cockroachSpeed(0.9));
