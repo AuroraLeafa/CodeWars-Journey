1baedfba8aa7f0730e15c91822c2ed76a73ace26
Refansyach <revansahdr@gmail.com>
2024-04-11 12:20:25 +0700

    Day 64 - Clear!

---
+++ 64-checkForFactor.js
@@ -0,0 +1,15 @@
+/* 
+    Day 64 - Code Wars Challenge
+    Link : https://www.codewars.com/kata/55cbc3586671f6aa070000fb/
+    CLEAR !!
+*/
+
+// function checkForFactor(base, factor) {
+//   return base % factor === 0;
+// }
+
+const checkForFactor = (base, factor) => base % factor === 0;
+
+console.log(checkForFactor(10, 2));
+console.log(checkForFactor(63, 7));
+console.log(checkForFactor(50, 8));
