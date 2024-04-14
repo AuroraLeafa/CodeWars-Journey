3439a87ec766c15946eb7dd22e6b857fa18bdd5a
Refansyach <revansahdr@gmail.com>
2024-04-14 09:46:56 +0700

    Day 67 - Clear!

---
+++ 67-getAge.js
@@ -0,0 +1,11 @@
+/* 
+    Day 67 - Code Wars Challenge
+    Link : https://www.codewars.com/kata/557cd6882bfa3c8a9f0000c1/
+    CLEAR !!
+*/
+
+function getAge(inputString) {
+  return parseInt(inputString);
+}
+
+console.log(getAge("4 years old"));
