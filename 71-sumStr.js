8bbc842d312a09b6cc01a3ebce1366ed8866b7fc
Refansyach <revansahdr@gmail.com>
2024-04-19 11:32:16 +0700

    Day 71 - Clear!

---
+++ 71-sumStr.js
@@ -0,0 +1,13 @@
+/* 
+    Day 71 - Code Wars Challenge
+    Link : https://www.codewars.com/kata/5966e33c4e686b508700002d
+    CLEAR!!
+*/
+
+// function sumStr(a,b) {
+//   return (Number(a) + Number(b)).toString()
+// }
+
+const sumStr = (a, b) => (+a + +b).toString()
+
+console.log(sumStr("5", ""));
