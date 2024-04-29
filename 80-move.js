e16d5b7c01582b3c14cc5d81876267c22473dfb0
Refansyach <revansahdr@gmail.com>
2024-04-29 17:00:33 +0700

    Day 80 - Clear!

---
+++ 80-move.js
@@ -0,0 +1,11 @@
+/* 
+    Day 80 - Code Wars Challenge
+    Link : https://www.codewars.com/kata/563a631f7cbbc236cf0000c2
+    CLEAR!!
+*/
+
+const move = (position, roll) => roll * 2 + position; 
+
+console.log(move(0, 4));
+console.log(move(3, 6));
+console.log(move(2, 5));
