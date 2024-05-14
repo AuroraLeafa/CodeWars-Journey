df787d8f058cc07ebeac6ba95c606c2dcd496c67
Refansyach <revansahdr@gmail.com>
2024-05-14 11:30:13 +0700

    Day 95 - Clear! (Catching up with my absence before)

---
+++ 95-xor.js
@@ -0,0 +1,13 @@
+/* 
+    Day 95 - Code Wars Challenge
+    Link : https://www.codewars.com/kata/56fa3c5ce4d45d2a52001b3c
+    CLEAR!!
+*/
+
+// function xor(a, b) {
+//     if(!a && b) return true
+//     if(a && !b) return true
+//     return false
+// }
+
+const xor = (a, b) => a != b
