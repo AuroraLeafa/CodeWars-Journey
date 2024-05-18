0cf6ab95c7aaf2a880ef9fc8cc2611da5138a8e5
Refansyach <revansahdr@gmail.com>
2024-05-18 19:07:42 +0700

    Day 99 - Clear! (1 more day till 100 YAYY)

---
+++ kata8/99-reverse.js
@@ -0,0 +1,15 @@
+/* 
+    Day 99 - Code Wars Challenge
+    Link : https://www.codewars.com/kata/57a55c8b72292d057b000594
+    CLEAR!!
+*/
+
+// function reverse(string) {}
+
+const reverse = (str) => str.split(" ").reverse().join(" ");
+
+console.log(reverse("My Name Is Revvvvvvvvv"));
+console.log(reverse("Your Name"));
+console.log(reverse("Weathering With You"));
+console.log(reverse("5CM Per Second"));
+console.log(reverse("Garden of Words"));
