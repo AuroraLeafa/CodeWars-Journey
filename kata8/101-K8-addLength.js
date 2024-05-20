d23733021880ebdcd504284517c1bd6a9aa411f5
Refansyach <revansahdr@gmail.com>
2024-05-20 14:10:22 +0700

    Day 101 - Clear ! (STILL DOING KATA 8 !!!)

---
+++ kata8/101-K8-addLength.js
@@ -0,0 +1,12 @@
+/* 
+    Kata 8
+    Day 101 - Code Wars Challenge
+    Link : https://www.codewars.com/kata/559d2284b5bb6799e9000047
+    CLEAR!!
+*/
+
+const addLength = (str) => str.split(" ").map(word => `${word} ${word.length}`)
+
+console.log(addLength("Lorem Ipsum Dolor Sit Amet"));
+console.log(addLength("TESTING ONE TWO THREE"));
+console.log(addLength("MY NAME'S JEFF"));
