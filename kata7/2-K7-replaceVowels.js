f99085b862d9ff00443e6c39b59c506ddaa10a89
Refansyach <revansahdr@gmail.com>
2024-05-21 22:24:21 +0700

    Day 2 - Kata 7 - Clear!

---
+++ kata7/2-K7-replaceVowels.js
@@ -0,0 +1,8 @@
+/* 
+    Kata 7
+    Day 2 - Code Wars Challenge
+    Link : https://www.codewars.com/kata/52fba66badcd10859f00097e
+    CLEAR!!
+*/
+
+const disemvowel = str => str.replace(/[aiueo]/g, "");
