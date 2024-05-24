802f3bc051c70e27becfe450cf3d92805669f3a1
Refansyach <revansahdr@gmail.com>
2024-05-24 11:29:18 +0700

    Fix: Missing Header

--- Kyu8/104-K8-hello.js
+++ Kyu8/104-K8-hello.js
@@ -1,3 +1,10 @@
+/* 
+    Kyu 8
+    Day 104 - Code Wars Challenge
+    Link : https://www.codewars.com/kata/57e3f79c9cb119374600046b
+    CLEAR!!
+*/
+
 const hello = name => `Hello, ${name ? name.charAt(0).toUpperCase() + name.slice(1).toLowerCase() + "!" : "World!"}`
 
 console.log(hello("joHn"));
