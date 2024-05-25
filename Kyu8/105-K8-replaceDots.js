8ef8d57859ce393782c19a631aef573f2a83b870
Refansyach <revansahdr@gmail.com>
2024-05-25 21:03:13 +0700

    Day 105 - K8 - Clear!

---
+++ Kyu8/105-K8-replaceDots.js
@@ -0,0 +1,9 @@
+/* 
+    Kyu 8
+    Day 105 - Code Wars Challenge
+    Link : https://www.codewars.com/kata/596c6eb85b0f515834000049
+    CLEAR!!
+*/
+const replaceDots = (str) => str.replaceAll(".", "-");
+
+console.log(replaceDots("One.Two.Three"));
