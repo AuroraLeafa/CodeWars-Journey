da384af873105a2bc6518e5984e00e5a24f7c35a
Refansyach <revansahdr@gmail.com>
2024-05-28 10:58:55 +0700

    Day 107 - K8 - Clear!

---
+++ Kyu8/107-K8-multiTable.js
@@ -0,0 +1,16 @@
+/* 
+    Kyu 8
+    Day 106 - Code Wars Challenge
+    Link : https://www.codewars.com/kata/5a2fd38b55519ed98f0000ce
+    CLEAR!!
+*/
+
+function multiTable(number) {
+  let res = ''
+  for (let i = 1; i <= 10; i++) {
+    res += `${i} * ${number} = ${i * number} ${i < 10 ? "\n" : ""}`;
+  }
+  return res
+}
+
+console.log(multiTable(3));
