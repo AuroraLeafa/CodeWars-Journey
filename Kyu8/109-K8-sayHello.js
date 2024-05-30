5949aca2d094dd3a936ad5897ce12b33a78d6b1d
Refansyach <revansahdr@gmail.com>
2024-05-30 11:20:28 +0700

    Day 109 - Kyu 8 - Clear!

---
+++ Kyu8/109-K8-sayHello.js
@@ -0,0 +1,12 @@
+/* 
+    Kyu 8
+    Day 103 - Code Wars Challenge
+    Link : https://www.codewars.com/kata/5302d846be2a9189af0001e4
+    CLEAR!!
+*/
+
+function sayHello(name, city, state) {
+  return `Hello, ${name.join(" ")}! Welcome to ${city.concat(", ", state)}!`;
+}
+
+console.log(sayHello(["John", "Smith"], "Phoenix", "Arizona"));
