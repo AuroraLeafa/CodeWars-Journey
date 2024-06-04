d38d6eddc404a40ed52a2e11edef10aeccb947c1
Refansyach <revansahdr@gmail.com>
2024-06-04 10:50:25 +0700

    Day 114 - Kyu 8 - Clear!

---
+++ Kyu8/114-K8-animal .js
@@ -0,0 +1,10 @@
+/* 
+    Kyu 8
+    Day 114 - Code Wars Challenge
+    Link : https://www.codewars.com/kata/571f1eb77e8954a812000837
+    CLEAR!!
+*/
+
+function animal(obj) {
+  return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
+}
