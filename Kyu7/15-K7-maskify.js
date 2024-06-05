08d37c81a744dbaa2685b994972f0801e3dbdee3
Refansyach <revansahdr@gmail.com>
2024-06-05 11:44:04 +0700

    Day 15 - Kyu7 - Clear!

---
+++ Kyu7/15-K7-maskify.js
@@ -0,0 +1,12 @@
+/* 
+    Kyu 7
+    Day 14 - Code Wars Challenge
+    Link : https://www.codewars.com/kata/5412509bd436bd33920011bc
+    CLEAR!!
+*/
+
+function maskify(cc) {
+  return cc.length <= 4 ? cc : "#".repeat(cc.length - 4) + cc.slice(-4);
+}
+
+console.log(maskify("My Name is Jeff"));
