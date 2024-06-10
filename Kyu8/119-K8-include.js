7b87c611271b9b4d953944d5a7990f32216e3b70
Refansyach <revansahdr@gmail.com>
2024-06-10 21:00:30 +0700

    Day 119 - Kyu 8 - Clear!

---
+++ Kyu8/119-K8-include.js
@@ -0,0 +1,11 @@
+/* 
+    Kyu 8
+    Day 119 - Code Wars Challenge
+    Link : https://www.codewars.com/kata/545991b4cbae2a5fda000158
+    CLEAR!!
+*/
+
+const include = (arr, item) => arr.includes(item);
+
+console.log(include([1, 2, 3, 4], 3));
+console.log(include([1, 2, 3, 4], 56));
