87b70746e9475dc6544f7266ed2e9d5bcf1e9d9b
Refansyach <revansahdr@gmail.com>
2024-06-12 11:05:30 +0700

    Day 22 - Kyu 7 - Clear!

--- Kyu7/22-K7-solution.js
+++ Kyu7/22-K7-solution.js
@@ -1,3 +1,10 @@
+/* 
+    Kyu 7
+    Day 22 - Code Wars Challenge
+    Link : https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d
+    CLEAR!!
+*/
+
 function solution(str, ending) {
   return str.endsWith(ending);
 }
