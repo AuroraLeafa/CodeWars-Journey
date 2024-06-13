ccee31807fe72e53ca34fe1fc5e698f8ac75ce6a
Refansyach <revansahdr@gmail.com>
2024-06-13 11:49:41 +0700

    Day 23 - Kyu 7 - Clear!

---
+++ Kyu7/23-K7-addBinary.js
@@ -0,0 +1,13 @@
+/* 
+    Kyu 7
+    Day 23 - Code Wars Challenge
+    Link : https://www.codewars.com/kata/551f37452ff852b7bd000139
+    CLEAR!!
+*/
+
+const addBinary = (a, b) => (a + b).toString(2);
+
+console.log(addBinary(5, 4));
+console.log(addBinary(10, 2));
+console.log(addBinary(1, 1));
+
