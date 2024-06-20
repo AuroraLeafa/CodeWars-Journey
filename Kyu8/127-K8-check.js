b3493e50f320b1b3fd0b4a4279fff2fe3b733e85
Refansyach <revansahdr@gmail.com>
2024-06-20 23:35:10 +0700

    Day 127 - Kyu 8 - Clear!

---
+++ Kyu8/127-K8-check.js
@@ -0,0 +1,10 @@
+/* 
+    Kyu 8
+    Day 126 - Code Wars Challenge
+    Link : https://www.codewars.com/kata/57241e0f440cd279b5000829
+    CLEAR!!
+*/
+
+const check = (a,x) => a.includes(x)
+
+console.log(check([66, 101], 66));
