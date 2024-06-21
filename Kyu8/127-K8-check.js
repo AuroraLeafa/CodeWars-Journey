7c41cf2ec2c4630649fe66d2def6e1cf574305ff
Refansyach <revansahdr@gmail.com>
2024-06-21 09:53:16 +0700

    Day 128 - Kyu 8 - Clear!

--- Kyu8/127-K8-check.js
+++ Kyu8/127-K8-check.js
@@ -1,6 +1,6 @@
 /* 
     Kyu 8
-    Day 126 - Code Wars Challenge
+    Day 127 - Code Wars Challenge
     Link : https://www.codewars.com/kata/57241e0f440cd279b5000829
     CLEAR!!
 */
---
+++ Kyu8/128-K8-printArray.js
@@ -0,0 +1,11 @@
+/* 
+    Kyu 8
+    Day 128 - Code Wars Challenge
+    Link : https://www.codewars.com/kata/56e2f59fb2ed128081001328
+    CLEAR!!
+*/
+
+
+const printArray = (array) => array.join(",");
+
+console.log(printArray(["h", "o", "l", "a"]));
