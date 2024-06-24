a8947b6a1d74090f9c92beb966e5017e4c1b4c1e
Refansyach <revansahdr@gmail.com>
2024-06-24 17:07:47 +0700

    Day 130 - Kyu 8 - Clear!

---
+++ Kyu8/130-K8-nearestSq .js
@@ -0,0 +1,8 @@
+/* 
+    Kyu 8
+    Day 130 - Code Wars Challenge
+    Link : https://www.codewars.com/kata/5a805d8cafa10f8b930005ba
+    CLEAR!!
+*/
+
+const nearestSq = n => Math.round(Math.sqrt(n)) ** 2
