c635e646f20ef17706c8004c734683598863728d
Refansyach <revansahdr@gmail.com>
2024-07-01 12:45:12 +0700

    Day 136 - Kyu 8 - Clear!

---
+++ Kyu8/136-K8-usdcny.js
@@ -0,0 +1,10 @@
+/* 
+    Kyu 8
+    Day 136 - Code Wars Challenge
+    Link : https://www.codewars.com/kata/5977618080ef220766000022
+    CLEAR!!
+*/
+
+const usdcny = usd => `${(usd * 6.75).toFixed(2)} Chinese Yuan`;
+
+console.log(usdcny(5));
