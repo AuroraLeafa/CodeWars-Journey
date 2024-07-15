690e4d7a6357d999dc0fc4b31efd2f8d80f36c4c
Refansyach <revansahdr@gmail.com>
2024-07-15 16:16:19 +0700

    Day 43 - Kyu 7 - Clear!

---
+++ Kyu7/43-K7-roundToNext5.js
@@ -0,0 +1,14 @@
+/* 
+    Kyu 7
+    Day 43 - Code Wars Challenge
+    Link : https://www.codewars.com/kata/55d1d6d5955ec6365400006d
+    CLEAR!!
+*/
+
+const roundToNext5 = n => Math.ceil(n / 5) * 5
+
+console.log(roundToNext5(0));
+console.log(roundToNext5(1));
+console.log(roundToNext5(2));
+console.log(roundToNext5(6));
+console.log(roundToNext5(23));
